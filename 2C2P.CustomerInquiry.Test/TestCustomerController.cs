8f727cd0b05ad8a4ec57dad16da23ee999b16b72
Jay. V <jay@fyin.com>
2019-01-03 10:45:42 +0500

    * Setup Initial Test

---
+++ 2C2P.CustomerInquiry.Test/TestCustomerController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _2C2P.CustomerInquiry.Core.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _2C2P.CustomerInquiry.Test
+{
+    [TestClass]
+    public class TestCustomerController
+    {
+        [TestMethod]
+        public void Transactions_ShouldReturnAllTransctions()
+        {
+            var transactions = TestTransactions();
+            var controller = new CustomerController();
+
+            //var result = controller.Transactions();
+            Assert.AreEqual(transactions.Count, 0);
+        }
+
+        private List<object> TestTransactions()
+        {
+            return new List<object>();
+        }
+    }
+}
